50a477717e765816e4e8605fce36b43006ce5b26
Drusketh <zenithdruskin@gmail.com>
2022-10-07 23:42:38 -0700

    first resource api endpoint made

--- includes/req.inc.php
+++ includes/req.inc.php
@@ -1,7 +1,23 @@
 <?php
-    $sql = "SELECT * FROM table WHERE ID = '$id'";
-    $result = $db->query($sql);
-    while ($row = $result->fetch_array()) {
-        echo $row['something'];
+    require_once "dbh.inc.php";
+
+    $sql = "SELECT * FROM nation WHERE uid = ?;";
+    $stmt = mysqli_stmt_init($ng);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=holyshitwtf");
+        exit();
     }
+    
+    $uid = 1;
+
+    mysqli_stmt_bind_param($stmt, "i", $uid);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $resultdata = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    echo json_encode($resultdata);
+
+    mysqli_stmt_close($stmt);
 ?>
