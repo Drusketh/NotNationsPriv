76f95afb2f5cdf1e043b0e7e9ac920da63942896
Drusketh <zenithdruskin@gmail.com>
2023-11-07 22:57:40 -0700

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 var card;
 var form = "<form class='factory faccard' action='includes/purchase.inc.php' method='POST' enctype='multipart/form-data'>";
 var button = "<button type='submit' name='submit' value='upload'>Purchase</button>";
-var count = "";
+var count = "<input class='form-number' type='number' name='count' placeholder='amount'><br>";
 
 // When the user clicks the button, open the modal 
 $(document).ready(function(){
@@ -14,6 +14,8 @@ $(document).ready(function(){
         $("div.modal-content").children(".factory").append(card); // add card display
 
         card.children()[9].remove(); // Remove purchase span
+
+        card.append(count);
 
         card.append(button); // add form purchase button
         modal.append("</form>");
